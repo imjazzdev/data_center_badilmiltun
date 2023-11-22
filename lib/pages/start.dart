a7bcee49df5ab7ab06f4c03edf1c66357c195156
ammar zafran <2003015018@uhamka.ac.id>
2023-11-22 15:33:35 +0700

    ammar 1

--- lib/pages/start.dart
+++ lib/pages/start.dart
@@ -5,6 +5,13 @@ class StartPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        body: Stack(
+      children: [
+        Column(
+          children: [Flexible(child: Text('ghghghh'))],
+        )
+      ],
+    ));
   }
 }
