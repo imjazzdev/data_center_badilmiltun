68b5a1db72e4c51e3343a0f3a7e54b5fa428c120
imjazzdev <jazzdev01@gmail.com>
2023-11-22 10:26:58 +0700

    fisrt commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:data_center_badilmiltun/pages/start.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: StartPage(),
+    );
+  }
+}
